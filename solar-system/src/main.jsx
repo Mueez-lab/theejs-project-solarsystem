1041c705d2bb8ed7b3c754f8bda9c623dcb76268
Mueez-lab <mueezzakir6@gmail.com>
2025-01-13 20:19:17 +0500

    update

--- solar-system/src/main.jsx
+++ solar-system/src/main.jsx
@@ -3,12 +3,100 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 const scene = new THREE.Scene()
 
+const textureLoader = new THREE.TextureLoader();
+const sunTexture = textureLoader.load('../textures/2k_sun.jpg');
+const earthTexture = textureLoader.load('../textures/2k_earth_daymap.jpg')
+// const moonTexture = textureLoader.load('../textures/2k_moon.jpg')
+const mercuryTexture = textureLoader.load('../textures/2k_mercury.jpg')
+const venusTexture = textureLoader.load('../textures/2k_venus_surface.jpg')
+const marsTexture = textureLoader.load('../textures/2k_mars.jpg')
+
 const sphereGeometry = new THREE.SphereGeometry(1,32,32)  // creating one generic 
-const sunMaterial = new THREE.MeshBasicMaterial({color:0xfff700})
+
+//SUN
+const sunMaterial = new THREE.MeshBasicMaterial({map:sunTexture})
+const earthMaterial = new THREE.MeshBasicMaterial({map:earthTexture})
+// const moonMaterial = new THREE.MeshBasicMaterial({map:moonTexture})
+const venusMaterial = new THREE.MeshBasicMaterial({map:venusTexture})
+const mercuryMaterial = new THREE.MeshBasicMaterial({map:mercuryTexture})
+const marsMaterial = new THREE.MeshBasicMaterial({map:marsTexture})
+
 const sun = new THREE.Mesh(sphereGeometry,sunMaterial)
-sun.scale.setScalar(5)
+sun.scale.setScalar(5) //109
 scene.add(sun)
 
+const planets = [
+  {
+    name: "Mercury",
+    radius: 0.5,
+    distance: 10,
+    speed: 0.01,
+    material: mercuryMaterial,
+    moons: [],
+  },
+  {
+    name: "Venus",
+    radius: 0.8,
+    distance: 15,
+    speed: 0.007,
+    material: venusMaterial,
+    moons: [],
+  },
+  {
+    name: "Earth",
+    radius: 1,
+    distance: 20,
+    speed: 0.005,
+    material: earthMaterial,
+    moons: [
+      {
+        name: "Moon",
+        radius: 0.3,
+        distance: 3,
+        speed: 0.015,
+      },
+    ],
+  },
+  {
+    name: "Mars",
+    radius: 0.7,
+    distance: 25,
+    speed: 0.003,
+    material: marsMaterial,
+    moons: [
+      {
+        name: "Phobos",
+        radius: 0.1,
+        distance: 2,
+        speed: 0.02,
+      },
+      {
+        name: "Deimos",
+        radius: 0.2,
+        distance: 3,
+        speed: 0.015,
+        color: 0xffffff,
+      },
+    ],
+  },
+];
+
+const planetMeshes = planets.map((planet)=>{
+  // create the mesh 
+  // add it in our scene 
+  // loop through each moon and create the moon
+  // add the moon to the planet 
+  const planetMesh = new THREE.Mesh(sphereGeometry,planet.material)
+  planetMesh.scale.setScalar(planet.radius)
+  planetMesh.position.x = planet.distance
+  scene.add(planetMesh)
+})
+
+console.log(planetMeshes);
+
+
+const amberLight = new THREE.AmbientLight(0xffffff,0.5);
+scene.add(amberLight)
 
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -16,7 +104,6 @@ const camera = new THREE.PerspectiveCamera(
   0.01,
   10000
 )
-
 
 camera.position.z = 100
 camera.position.y = 5
@@ -32,7 +119,7 @@ renderer.setPixelRatio(Math.max(window.devicePixelRatio, 2)); // to make the ima
 // initialize controls after renderer
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true; // to make the movement smooth
-controls.autoRotate = true; // to make the camera auto rotate
+controls.autoRotate = false; // to make the camera auto rotate
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight; 
@@ -42,8 +129,11 @@ window.addEventListener('resize', () => {
 }); // resize event listener
 
 
+const clock = new  THREE.Clock();
 
 const renderLoop = () =>{
+  const elapsedTime = clock.getElapsedTime();  
+  sun.rotation.y+=0.0003
   controls.update(); // update controls in every frame
   renderer.render(scene, camera); // render once display what was in image as a pic one time but we want render loop so that we can see the animation but to do that we need requestAnimationFrame
   window.requestAnimationFrame(renderLoop)
